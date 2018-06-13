b4f4926253b336acc8a62cde37e0f39fbafb6345
Dayananda D.R <Dayananda.DR@emc.com>
2018-06-13 20:37:01 +0530

    Adding few important concepts

---
+++ Iterators/HowitImplemented.py
@@ -0,0 +1,24 @@
+class PowTwo:
+    """Class to implement an iterator
+    of powers of two"""
+
+    def __init__(self, max = 0):
+        self.max = max
+
+    def __iter__(self):
+        self.n = 0
+        return self
+
+    def __next__(self):
+        if self.n <= self.max:
+            result = 2 ** self.n
+            self.n += 1
+            return result
+        else:
+            raise StopIteration
+
+if __name__ == "__main__":
+    element = PowTwo(5)
+    item = iter(element)
+    next(item)
+
