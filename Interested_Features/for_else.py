d2fc330bc552a9c8eb71c1507957ae7da38c52ec
Dayananda D.R <Dayananda.DR@emc.com>
2018-08-21 12:32:53 +0530

    few interesting features

---
+++ Interested_Features/for_else.py
@@ -0,0 +1,15 @@
+""""
+for loops also have an else clause.
+The else clause executes after the loop completes normally.
+This means that the loop did not encounter a break statement.
+One Common Usage is to check whether loop ends or not.
+we can check it via flag but for else is preferred way
+"""
+
+fruits = ['Apple','banana','orange']
+for fruit in fruits:
+    if fruit == 'Mango':
+        print "It is a Mangoo Season and It is the king of the fruit"
+        break
+else:
+    print "This is not a Mango Season !!!! of Course it is the king of the fruit"
---
+++ Strings/StringLiterals.py
@@ -0,0 +1,24 @@
+print("This is how we do backslash in Python  \\")
+print("To display double backslash in python two extra escape backslash are required \\\\")
+
+print(r'To display double backslash in using rawstring \\')
+
+print(r'To display single backslash in using rawstring \ ')
+
+
+
+print('Let\'s Learn something new in Python')
+
+
+print("\"To escape double quotes\"")
+
+
+print("123\b456 backslash to delete number 3")
+
+
+print("tab escape \t  \t shown here")
+
+print("This is my firt line \n This is my second line \n this is my third line")
+
+
+print("this is to show \v vertical tab \v another line")
