2d099f464c31e298beb13a8e6b486dc891519ce2
Dayananda D.R <Dayananda.DR@emc.com>
2018-10-05 17:20:25 +0530

    Adding Streaming Logs Subprocess module

---
+++ Subprocess/Streaming_logs.py
@@ -0,0 +1,15 @@
+from __future__ import print_function # Only Python 2.x
+import subprocess
+
+def execute(cmd):
+    popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
+    for stdout_line in iter(popen.stdout.readline, ""):
+        yield stdout_line
+    popen.stdout.close()
+    return_code = popen.wait()
+    if return_code:
+        raise subprocess.CalledProcessError(return_code, cmd)
+
+# Example
+for path in execute(["tail","-f", "/var/log/messages"]):
+    print(path, end="")
