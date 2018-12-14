87622db6718eec05e50842e8f11ceaa5d0ef5de8
DR <Dayananda_DR@McAfee.com>
2018-12-14 11:36:39 +0530

    refined Generators

--- Generators/PythonGeneratorsWithLoop.py
+++
@@ -1,8 +0,0 @@
-def rev_str(my_str):
-    length = len(my_str)
-    for i in range(length - 1,-1,-1):
-        yield my_str[i]
-
-if __name__ == '__main__':
-    for item in rev_str("GoodMorning"):
-        print item
